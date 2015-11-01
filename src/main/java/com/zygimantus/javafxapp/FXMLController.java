60288e9fea14748cfd560abe8df86fead8f06e13
zygimantus <zygimantus@gmail.com>
2015-11-01 13:50:26 +0200

    logger added

--- src/main/java/com/zygimantus/javafxapp/FXMLController.java
+++ src/main/java/com/zygimantus/javafxapp/FXMLController.java
@@ -6,15 +6,21 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 
 public class FXMLController implements Initializable {
+
+    private static final Logger logger = LogManager.getLogger();
 
     @FXML
     private Label label;
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
+        logger.info("You clicked me!");
         label.setText("Hello World!");
     }
 
